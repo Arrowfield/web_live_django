346f678a496998df08fc97d7cae1e1fdcaa51e36
field <768449566@qq.com>
2019-08-12 16:30:24 +0800

    update

--- live/views.py
+++ live/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Question
+
+from django.template import loader
+
 # Create your views here.
 def index(request):
-  return HttpResponse('Hello,world . you are at index view ')
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  template = loader.get_template('live/index.html')
+  context = {
+    'latest_question_list':latest_question_list,
+  }
+
+  #output = ', '.join([q.question_text for q in latest_question_list])
+  
+  return HttpResponse(template.render(context,request))
+  #return HttpResponse('Hello,world . you are at index view ')
 
 def detail(request,question_id):
   return HttpResponse('you are looking at question %s' % question_id) 
