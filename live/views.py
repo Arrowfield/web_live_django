f326878102861a112942502b1cca4b1019e460c3
field <768449566@qq.com>
2019-08-12 16:34:03 +0800

    update

--- live/views.py
+++ live/views.py
@@ -9,15 +9,18 @@ from django.template import loader
 # Create your views here.
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  template = loader.get_template('live/index.html')
+  #template = loader.get_template('live/index.html')
+  
   context = {
     'latest_question_list':latest_question_list,
   }
 
   #output = ', '.join([q.question_text for q in latest_question_list])
   
-  return HttpResponse(template.render(context,request))
+  #return HttpResponse(template.render(context,request))
+  
   #return HttpResponse('Hello,world . you are at index view ')
+  return render(request,'live/index.html',context)
 
 def detail(request,question_id):
   return HttpResponse('you are looking at question %s' % question_id) 
